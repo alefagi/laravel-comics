9009867d8aecd49fcee770ac76242d8d31003d7d
alefagi <iltu@libero.it>
2021-10-01 17:17:00 +0200

    added include for header and footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,68 @@
+<footer>
+  <section id="links">
+    <div class="container">
+      <div class="list">
+        <div>
+          <h4>dc comics</h4>
+          <ul>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
+          <h4>shop</h4>
+          <ul>
+            <li><a href="#">Shop DC</a></li>
+            <li><a href="#">Shop DC Collectibles</a></li>
+          </ul>
+        </div>
+        <div>
+          <h4>dc</h4>
+          <ul>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Privacy Policy (New)</a></li>
+            <li><a href="#">Ad Choices</a></li>
+            <li><a href="#">Advertising</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Subscriptions</a></li>
+            <li><a href="#">Talent Workshops</a></li>
+            <li><a href="#">CPSC Certificates</a></li>
+            <li><a href="#">Ratings</a></li>
+            <li><a href="#">Shop Help</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+        </div>
+        <div>
+          <h4>sites</h4>
+          <ul>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD Magazine</a></li>
+            <li><a href="#">DC Kids</a></li>
+            <li><a href="#">DC Universe</a></li>
+            <li><a href="#">DC Power Visa</a></li>
+          </ul>
+        </div>
+      </div>
+      <figure>
+        <img src="{{ asset('images/dc-logo-bg.png') }}" alt="logo-bg">
+      </figure>
+    </div>
+  </section>
+
+  <section id="social">
+    <div class="container">
+      <button>sign-up now!</button>
+      <div>
+        <span>follow us</span>
+        <a href="#"><img src="{{ asset('images/footer-facebook.png') }}" alt="facebook"></a>
+        <a href="#"><img src="{{ asset('images/footer-twitter.png') }}" alt="twitter"></a>
+        <a href="#"><img src="{{ asset('images/footer-youtube.png') }}" alt="youtube"></a>
+        <a href="#"><img src="{{ asset('images/footer-pinterest.png') }}" alt="pinterest"></a>
+        <a href="#"><img src="{{ asset('images/footer-periscope.png') }}" alt="periscope"></a>
+      </div>
+    </div>
+  </section>
+</footer>
