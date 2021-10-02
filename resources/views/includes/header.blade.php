47259413aa8aba9ccab29087c69e66819a536ad5
alefagi <iltu@libero.it>
2021-10-02 20:10:54 +0200

    modified header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,19 +1,30 @@
-<header class="container">
-  <figure>
-    <img src="{{ asset('images/dc-logo.png') }}" alt="dc-logo">
-  </figure>
-  <nav>
-    <ul>
-      <li><a href="#">Characters</a></li>
-      <li><a href="#">Comics</a></li>
-      <li><a href="#">Movies</a></li>
-      <li><a href="#">TV</a></li>
-      <li><a href="#">Games</a></li>
-      <li><a href="#">Collectibles</a></li>
-      <li><a href="#">Videos</a></li>
-      <li><a href="#">Fans</a></li>
-      <li><a href="#">News</a></li>
-      <li><a href="#">Shop</a></li>
-    </ul>
-  </nav>
+<header>
+  <div class="top-bar">
+    <div class="container">
+      <a href="#">DC POWER VISA&#174;</a>
+      <a href="#">ADDITIONAL DC SITES &#11206;</a>
+    </div>
+  </div>
+  <div class="bottom-bar container">
+    <figure>
+      <img src="{{ asset('images/dc-logo.png') }}" alt="dc-logo">
+    </figure>
+    <nav>
+      <ul>
+        <li><a href="#">Characters</a></li>
+        <li><a href="#">Comics</a></li>
+        <li><a href="#">Movies</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">Games</a></li>
+        <li><a href="#">Collectibles</a></li>
+        <li><a href="#">Videos</a></li>
+        <li><a href="#">Fans</a></li>
+        <li><a href="#">News</a></li>
+        <li><a href="#">Shop <span>&#9207;</span></a></li>
+      </ul>
+    </nav>
+    <div>
+      <input type="text" placeholder="Search"><i class="fas fa-search"></i>
+    </div>
+  </div>
 </header>
