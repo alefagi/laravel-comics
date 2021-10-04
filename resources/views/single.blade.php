13bfa77379f5c93a25da56e8625822b2e68954c8
alefagi <iltu@libero.it>
2021-10-04 20:18:11 +0200

    added serie-details

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -25,5 +25,64 @@
     </div>
   </div>
 
-  <div class="serie-details"></div>
+  <div class="serie-details">
+    <section id="details-top">
+      <div class="container-small">
+        <div class="talent">
+          <h3>Talent</h3>
+          <div class="detail">
+            <h5>Art by:</h5>
+            <p class="primary">{{ $series[0]['artists'][0]}}</p>
+          </div>
+          <div class="detail">
+            <h5>Written by:</h5>
+            <p class="primary">{{ $series[0]['writers'][0]}}</p>
+          </div>
+        </div>
+        <div class="specs">
+          <h3>Specs</h3>
+          <div class="detail">
+            <h5>Series:</h5>
+            <p class="primary">{{ $series[0]['series']}}</p>
+          </div>
+          <div class="detail">
+            <h5>U.S. Price:</h5>
+            <p>{{ $series[0]['price']}}</p>
+          </div>
+          <div class="detail">
+            <h5>On Sale Date:</h5>
+            <p>{{ $series[0]['sale_date']}}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="details-bottom">
+      <div class="container-small">
+        <a href="#">
+          <span>DIGITAL COMICS</span>
+          <figure>
+            <img src="{{ asset('images/buy-comics-digital-comics.png')}}" alt="digital-comics">
+          </figure>
+        </a>
+        <a href="#">
+          <span>SHOP DC</span>
+          <figure>
+            <img src="{{ asset('images/buy-comics-subscriptions.png')}}" alt="merchandise">
+          </figure>
+        </a>
+        <a href="#">
+          <span>COMIC SHOP LOCATOR</span>
+          <figure>
+            <img src="{{ asset('images/buy-comics-shop-locator.png')}}" alt="shop-locator">
+          </figure>
+        </a>
+        <a href="#">
+          <span>SUBSCRIPTIONS</span>
+          <figure>
+            <img src="{{ asset('images/buy-comics-merchandise.png')}}" alt="subscriptions">
+          </figure>
+        </a>
+      </div>
+    </section>  
+  </div>
 @endsection
